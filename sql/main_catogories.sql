faa32b364850cb40f1084fdaabc3900739a39d45
Darshana <keerthi.mac@gmail.com>
2022-11-29 21:04:14 +0530

    SQL UPDATED

--- sql/main_catogories.sql
+++ sql/main_catogories.sql
@@ -28,32 +28,32 @@ use arch_database;
 
 
 CREATE TABLE sub_contract_company(
-    sub_contract_company_id INT NOT NULL,
+    sub_contract_company_id INT NOT NULL AUTO_INCREMENT,
     sub_contract_company_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (sub_contract_company_id)
 );
 
 CREATE TABLE project(
-    project_id INT NOT NULL,
+    project_id INT NOT NULL AUTO_INCREMENT,
     project_name VARCHAR(50) NOT NULL,
     client_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (project_id)
 );
 
 CREATE TABLE project_stage(
-    project_stage_id INT NOT NULL,
+    project_stage_id INT NOT NULL AUTO_INCREMENT,
     project_stage VARCHAR(50) NOT NULL,
     PRIMARY KEY (project_stage_id)
 );
 
 CREATE TABLE element(
-    element_id INT NOT NULL,
+    element_id INT NOT NULL AUTO_INCREMENT,
     element_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (element_id)
 );
 
 CREATE TABLE sub_contract(
-    sub_contract_id INT NOT NULL,
+    sub_contract_id INT NOT NULL AUTO_INCREMENT,
     sub_contract_company_id INT NOT NULL,
     project_id INT NOT NULL,
     project_stage_id INT NOT NULL,
@@ -65,7 +65,7 @@ CREATE TABLE sub_contract(
 
 
 CREATE TABLE sub_contract_work (
-    sub_contract_work_id INT NOT NULL,
+    sub_contract_work_id INT NOT NULL AUTO_INCREMENT,
     element_id INT NOT NULL,
     project_stage_id INT NOT NULL,
     sub_contract_work VARCHAR(50) NOT NULL,
